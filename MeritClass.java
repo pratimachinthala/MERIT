970c722240bafde1efa11534eec58ecb43319d64
pratimachinthala <pratima.chinthala@gmail.com>
2018-03-15 13:38:16 -0700

    init javaclass

--- MeritClass.java
+++ MeritClass.java
@@ -2,9 +2,9 @@
 public class MeritClass {
 
 	public static void main(String[] args) {
-System.out.println("This is merit project");
-System.out.println("This is merit project");
-System.out.println("This is merit Line 3");
+System.out.println("This is merit 1");
+System.out.println("This is merit 2");
+System.out.println("This is merit 3");
 
 
 	}
